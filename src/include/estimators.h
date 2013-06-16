945b00123bf13fa108fc4aafc52c64f87836aaef
evilncrazy <user2@server.fake>
2013-06-16 15:01:57 +1000

    Add functions to perform simple approximate inference

---
+++ src/include/estimators.h
@@ -0,0 +1,24 @@
+#ifndef GRIDINF_INCLUDE_ESTIMATORS_H
+#define GRIDINF_INCLUDE_ESTIMATORS_H
+
+#include "core.h"
+
+namespace ginf {
+	// Find the labeling where every node chooses the label that minimizes the data cost
+	template <typename T>
+	void decodeMaxPrior(Grid<T> *grid, Matrix<int> *result) {
+		for (int y = 0; y < grid->getHeight(); y++) {
+			for (int x = 0; x < grid->getWidth(); x++) {
+				int minLabel = 0;
+				for (int i = 1; i < grid->getNumLabels(); i++) {
+					if (grid->getDataCost(x, y, i) < grid->getDataCost(x, y, minLabel)) {
+						result->get(x, y) = i;
+						minLabel = i;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
