700b58984ef52871d8798c2b68785692f7e58ac3
evilncrazy <user2@server.fake>
2013-06-22 19:50:18 +1000

    Implement optimizations for belief propagation

--- src/gpu/bp.cu
+++ src/gpu/bp.cu
@@ -2,75 +2,65 @@
 
 namespace ginf {
 	template <typename T>
-	__device__ void gpuBpCollect(GpuGrid<T> *grid, int w, int h, T *msgs, T *from, int x, int y) {
+	__global__ void gpuBpCollectKernel(GpuGrid<T> *grid, int w, int h, T *msgs, T *from, T *data) {
+		GINF_DECL_X; GINF_DECL_Y;
+	
 		MatDim dim(w, h);
 		MatDim dimMsgs(w, h, GINF_NUM_DIR);
 
-		// For each state
-		for (int i = 0; i < grid->getNumLabels(); i++) {
-			from[dim.idx(x, y, i)] = 0;
-		
-			// For each neighbour in each direction, sum up the messages
-			for (int d = 0; d < GINF_NUM_DIR; d++) {
-				from[dim.idx(x, y, i)] += msgs[dimMsgs.idx(x + dDirX[d], y + dDirY[d], GINF_OPP_DIR(d), i)];
+		if (dim.isValidStrict(x, y)) {
+			// For each state
+			for (int i = 0; i < grid->getNumLabels(); ++i) {
+				from[dim(x, y, i)] = data[dim(x, y, i)];
+				
+				// For each neighbour in each direction, sum up the messages
+				for (int d = 0; d < GINF_NUM_DIR; d++) {
+					from[dim(x, y, i)] += msgs[dimMsgs(x, y, d, i)];
+				}
 			}
 		}
 	}
 	
 	template <typename T>
-	__device__ void gpuBpSendMsg(int iter, GpuGrid<T> *grid, int w, int h, T *msgs, T *from, T *data, T *out, int d) {
+	__global__ void gpuBpKernel(int iter, GpuGrid<T> *grid, int w, int h, T *msgs, T *from, T *data, int d) {
 		GINF_DECL_ALT_X(iter); GINF_DECL_Y;
-		MatDim dim(w, h);
+		
+		MatDim dim(w, h, GINF_NUM_DIR);
 		MatDim dimMsgs(w, h, GINF_NUM_DIR);
 
-	   	int nx = x + dDirX[d], ny = y + dDirY[d];
-
-		T minh = out[0] = data[dim.idx(x, y, 0)] + (from[dim.idx(x, y, 0)] - msgs[dimMsgs.idx(nx, ny, GINF_OPP_DIR(d), 0)]);
-	    	for (int i = 1; i < grid->getNumLabels(); i++) {
-	    		out[i] = data[dim.idx(x, y, i)] + (from[dim.idx(x, y, i)] - msgs[dimMsgs.idx(nx, ny, GINF_OPP_DIR(d), i)]);
-	    		minh = GINF_MIN(minh, out[i]);
-	    	}
-	    	
-	    	// Find the linear scaling constant
-		T scale = grid->getSmoothnessCost(0, 1);
-		for (int i = 1; i < grid->getNumLabels(); i++)
-			out[i] = GINF_MIN(out[i], out[i - 1] + scale);
-		for (int i = grid->getNumLabels() - 2; i >= 0; i--)
-			out[i] = GINF_MIN(out[i], out[i + 1] + scale);
-	    	
-		// Truncate the messages using truncate constant
-		minh += GINF_MIN(grid->getSmoothnessCost(0, 1) * grid->getNumLabels(),
-				grid->getSmoothnessCost(0, grid->getNumLabels() - 1));
-		T val = 0;
-		for (int i = 0; i < grid->getNumLabels(); i++) {
-			out[i] = GINF_MIN(out[i], minh);
-			val += out[i];
-		}
-		
-		// Normalize
-		val /= grid->getNumLabels();
-		for (int i = 0; i < grid->getNumLabels(); i++) {
-			out[i] -= val;
-		}
-	}
-	
-	template <typename T>
-	__global__ void gpuBpKernel(int iter, GpuGrid<T> *grid, int w, int h, T *msgs, T *from, T *data) {
-		GINF_DECL_ALT_X(iter); GINF_DECL_Y;
-		MatDim dim(w, h, GINF_NUM_DIR);
-		
 		if (dim.isValidStrict(x, y)) {
-			// Collect messages from neighbours
-			gpuBpCollect(grid, w, h, msgs, from, x, y);
-			
-			// Foreach direction, send message
+			// Create a vector to store the message. This is stored in local memory, and so writing to it
+			// is coalesced.
 			T out[GINF_MAX_NUM_LABELS];
-			for (int d = 0; d < GINF_NUM_DIR; d++) {
-				gpuBpSendMsg(iter, grid, w, h, msgs, from, data, out, d);
+			
+			// Find the linear scaling constant
+			T scale = grid->getSmoothnessCost(0, 1);
+			
+			// Calculate message vector in the forward direction
+			T minh = out[0] = from[dim(x, y, 0)] - msgs[dimMsgs(x, y, d, 0)];
+			for (int i = 1; i < grid->getNumLabels(); i++) {
+				out[i] = GINF_MIN(from[dim(x, y, i)] - msgs[dimMsgs(x, y, d, i)], out[i - 1] + scale);
+				minh = GINF_MIN(minh, out[i]);
+			}
 				
-				for (int i = 0; i < grid->getNumLabels(); i++) {
-					msgs[dim.idx(x, y, d, i)] = out[i];
-				}
+			// Truncate the messages using truncate constant
+			minh += grid->getSmoothnessCost(0, grid->getNumLabels() - 1);
+				
+			// Compute the final out vector, and sum up all the elements to perform normalization
+			T val = 0;
+			for (int i = grid->getNumLabels() - 2; i >= 0; i--) {
+				out[i] = GINF_MIN(GINF_MIN(out[i], out[i + 1] + scale), minh);
+				val += out[i];
+			}
+
+			// Find the normalizing constant
+			val /= grid->getNumLabels();
+
+			// Update the message
+			int nx = x + dDirX[d], ny = y + dDirY[d];
+			for (int i = 0; i < grid->getNumLabels(); i++) {
+				// Update using the normalized message values
+				msgs[dim(nx, ny, GINF_OPP_DIR(d), i)] = out[i] - val;
 			}
 		}
 	}
@@ -79,18 +69,14 @@ namespace ginf {
 	__global__ void gpuBpGetBeliefKernel(GpuGrid<T> *grid, T *msgs, T *from, int *result) {
 		GINF_DECL_X; GINF_DECL_Y;
 		MatDim dim(grid->getWidth(), grid->getHeight());
-		MatDim dimMsgs(grid->getWidth(), grid->getHeight(), GINF_NUM_DIR);
 		
 		if (dim.isValidStrict(x, y)) {
-			// Collect msgs from neighbours
-			gpuBpCollect(grid, grid->getWidth(), grid->getHeight(), msgs, from, x, y);
-		
 			// Pick the label that minimizes the cost
-			T minCost = grid->getDataCost(x, y, 0) + from[dim.idx(x, y, 0)];
+			T minCost = from[dim(x, y, 0)];
 			int minLabel = 0;
 			
-			for (int i = 1; i < grid->getNumLabels(); i++) {
-				T cost = grid->getDataCost(x, y, i) + from[dim.idx(x, y, i)];
+			for (int i = 1; i < grid->getNumLabels(); ++i) {
+				T cost = from[dim(x, y, i)];
 				if (cost < minCost) {
 					minCost = cost;
 					minLabel = i;
@@ -98,7 +84,7 @@ namespace ginf {
 			}
 			
 			// Write the result
-			result[dim.idx(x, y)] = minLabel;
+			result[dim(x, y)] = minLabel;
 		}
 	}
 	
@@ -126,7 +112,7 @@ namespace ginf {
 			for (int d = 0; d < GINF_NUM_DIR; d++) {
 				for (int i = 0; i < grid->getNumLabels(); i++) {
 					// Initialize message with node on the above level
-					msgs[dim.idx(x, y, d, i)] = prev[dimPrev.idx(x / 2, y / 2, d, i)];
+					msgs[dim(x, y, d, i)] = prev[dimPrev(x / 2, y / 2, d, i)];
 				}
 			}
 		}
@@ -137,7 +123,7 @@ namespace ginf {
 		Gpu gpu;
 		
 		// Create matrices to store data costs at each level
-            	Matrix<T> **data = new Matrix<T>* [numLevels];
+		Matrix<T> **data = new Matrix<T>* [numLevels];
 		
 		// Allocate memory on the device
 		int *dResult = gpu.createRawMat(result);
@@ -155,11 +141,10 @@ namespace ginf {
 		
 		// Copy it to the device
 		dData[0] = gpu.createRawMat(data[0]);
-           
-           	// Prepare block sizes
-		dim3 blockSize(GINF_BP_BLOCK_SIZE, GINF_BP_BLOCK_SIZE);
-		dim3 gridSize(GINF_DIM(grid->getWidth(), GINF_BP_BLOCK_SIZE), 
-			      GINF_DIM(grid->getHeight(), GINF_BP_BLOCK_SIZE));
+
+		// Prepare block sizes
+		dim3 blockSize(GINF_BP_BLOCK_WIDTH, GINF_BP_BLOCK_HEIGHT);
+		dim3 gridSize(GINF_DIM(grid->getWidth(), blockSize.x), GINF_DIM(grid->getHeight(), blockSize.y));
 
 		// Calculate the data costs on the other levels
 		for (int t = 1; t < numLevels; t++) {
@@ -169,7 +154,7 @@ namespace ginf {
 			// We gotta make sure that we don't get too 'coarse'
 			if (w < 1 || h < 1) return;
 
-			// The data cost of each node on the current level uses the sum of
+			// The data cost of each node GINF_BP_BLOCK_WIDTHon the current level uses the sum of
 			// the data costs of four nodes below this level
 			data[t] = new Matrix<T>(3, w, h, grid->getNumLabels());
 			for (int y = 0; y < data[t - 1]->getSize(1); y++) {
@@ -204,12 +189,19 @@ namespace ginf {
 			}
 			
 			// For each iteration, run belief propagation
+			dim3 lvlGridSize(GINF_DIM(w / 2 + 1, blockSize.x), GINF_DIM(h, blockSize.y));
 			for (int i = 0; i < numItersPerLevel; i++) {
-				gpuBpKernel<<<gridSize, blockSize>>>(i, dGrid, w, h, dMsgs[t], dFrom[t], dData[t]);
+				gpuBpCollectKernel<<<gridSize, blockSize>>>(dGrid, w, h, dMsgs[t], dFrom[t], dData[t]);
+
+				// For each direction, send a message
+				for (int d = 0; d < GINF_NUM_DIR; d++) {
+					gpuBpKernel<<<lvlGridSize, blockSize>>>(i, dGrid, w, h, dMsgs[t], dFrom[t], dData[t], d);
+				}
 			}
 		}
 		
 		// Finally get the beliefs
+		gpuBpCollectKernel<<<gridSize, blockSize>>>(dGrid, grid->getWidth(), grid->getHeight(), dMsgs[0], dFrom[0], dData[0]);
 		gpuBpGetBeliefKernel<<<gridSize, blockSize>>>(dGrid, dMsgs[0], dFrom[0], dResult);
 		
 		// Transfer the results back to CPU
@@ -230,10 +222,10 @@ namespace ginf {
 	template void gpuDecodeHbp<int>(Grid<int>*, int, int, Matrix<int>*);
 	
 	template <typename T>
-	void gpuDecodeBpSync(Grid<T> *grid, int numIters, Matrix<int> *result) {
+	void gpuDecodeBp(Grid<T> *grid, int numIters, Matrix<int> *result) {
 		gpuDecodeHbp(grid, 1, numIters, result);
 	}
 	
-	template void gpuDecodeBpSync<float>(Grid<float>*, int, Matrix<int>*);
-	template void gpuDecodeBpSync<int>(Grid<int>*, int, Matrix<int>*);
+	template void gpuDecodeBp<float>(Grid<float>*, int, Matrix<int>*);
+	template void gpuDecodeBp<int>(Grid<int>*, int, Matrix<int>*);
 }
