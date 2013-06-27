234496d6e5905b6eea75d2e41e75d899fa88d399
evilncrazy <user2@server.fake>
2013-06-27 20:29:20 +1000

    Add random estimator

--- src/include/estimators.h
+++ src/include/estimators.h
@@ -3,6 +3,8 @@
 
 #include "core.h"
 
+#include <cstdlib>
+
 namespace ginf {
 	// Find the labeling where every node chooses the label that minimizes the data cost
 	template <typename T>
@@ -18,6 +20,16 @@ namespace ginf {
 				}
 				
 				result->get(x, y) = minLabel;
+			}
+		}
+	}
+	
+	// Generate a random labeling
+	template <typename T>
+	void decodeRandom(Grid<T> *grid, Matrix<int> *result) {
+		for (int y = 0; y < grid->getHeight(); y++) {
+			for (int x = 0; x < grid->getWidth(); x++) {
+				result->get(x, y) = int(rand() * 1.0 / RAND_MAX * grid->getNumLabels());
 			}
 		}
 	}
